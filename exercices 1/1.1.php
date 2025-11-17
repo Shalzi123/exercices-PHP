2a1d3562748ad03246ebad4de37f325b7cd395d4
Shalzi <warsbot160@gmail.com>
2025-11-17 14:54:56 +0100

    Add files via upload

---
+++ exercices 1/1.1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+function schoolLevel($age) {
+    if ($age < 3) {
+        return "creche";
+    } elseif ($age < 6) {
+        return "maternelle";
+    } elseif ($age < 11) {
+        return "primaire";
+    } elseif ($age < 16) {
+        return "college";
+    } elseif ($age < 18) {
+        return "lycee";
+    } else {
+        return "";
+    }
+}
+
+
+
+echo "<h3>Exercice 1 - School</h3>";
+echo schoolLevel(4) . "<br>";  //  maternelle
+echo schoolLevel(10) . "<br>"; //  primaire
+
+
+?>
+
+</body>
+</html>
