bfa9b79875452ac879e8ab7ca045bf47dcbd8b7e
sacha <sacharibolzi712@gmail.com>
2025-11-17 11:30:15 +0100

    first commit

--- 1.1.php
+++ 1.1.php
@@ -26,42 +26,10 @@ function schoolLevel($age) {
 
 
 
-function fooBar() {
-    for ($i = 1; $i <= 100; $i++) {
-        if ($i % 15 == 0) {
-            echo "FooBar<br>";
-        } elseif ($i % 3 == 0) {
-            echo "Foo<br>";
-        } elseif ($i % 5 == 0) {
-            echo "Bar<br>";
-        } else {
-            echo $i . "<br>";
-        }
-    }
-}
-
-
-function printPattern($n) {
-    for ($i = 1; $i <= $n; $i++) {
-        for ($j = 1; $j <= $i; $j++) {
-            echo $i;
-        }
-        echo "<br>";
-    }
-}
-
-
-
-
 echo "<h3>Exercice 1 - School</h3>";
 echo schoolLevel(4) . "<br>";  // Ex: maternelle
 echo schoolLevel(10) . "<br>"; // Ex: primaire
 
-echo "<h3>Exercice 2 - Foo Bar</h3>";
-fooBar();
-
-echo "<h3>Exercice 3 - Double boucle</h3>";
-printPattern(5);
 
 ?>
 
---
+++ 1.3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+function printPattern($n) {
+    for ($i = 1; $i <= $n; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo $i;
+        }
+        echo "<br>";
+    }
+}
+
+// Exemple
+printPattern(5);
+?>
+
+</body>
+</html>
